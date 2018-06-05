87d866802c2797468f10767896bb9dacf42c0b23
Craig Fleming <rythos42@gmail.com>
2018-06-05 11:03:35 -0700

    Adding undo button, fixing some warnings

--- app/src/main/java/com/geeksong/russianrailroadsscorer/CalculatorActivity.java
+++ app/src/main/java/com/geeksong/russianrailroadsscorer/CalculatorActivity.java
@@ -1,7 +1,9 @@
 package com.geeksong.russianrailroadsscorer;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -9,7 +11,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.v7.app.NotificationCompat;
+import android.support.v4.app.NotificationCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
@@ -21,12 +23,15 @@ import android.widget.NumberPicker;
 import android.widget.RemoteViews;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class CalculatorActivity extends Activity {
     private int currentScore = 0;
     private boolean isDoubled = false;
     private boolean isUsingRevaluationMarker = false;
     private boolean isUsingGermanRevaluationMarker = false;
     private String refreshStartCardsAction = "refresh-start-cards";
+    private ArrayList<Integer> history = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,8 +50,8 @@ public class CalculatorActivity extends Activity {
     }
 
     private void updateScoreDisplay() {
-        TextView scoreValue = (TextView) findViewById(R.id.scoreValue);
-        scoreValue.setText(Integer.toString(currentScore));
+        TextView scoreValue = findViewById(R.id.scoreValue);
+        scoreValue.setText(String.format("%d", currentScore));
     }
 
     private void addToDisplayedScore(int add, ScoreType type) {
@@ -58,21 +63,48 @@ public class CalculatorActivity extends Activity {
     private void addToHistory(int add, ScoreType type) {
         View addedAction = AddedActionFactory.create(add, type, isDoubled, isUsingRevaluationMarker, isUsingGermanRevaluationMarker);
 
-        final LinearLayout previousActionsLayout = (LinearLayout) findViewById(R.id.previousActions);
+        final LinearLayout previousActionsLayout = findViewById(R.id.previousActions);
         previousActionsLayout.addView(addedAction);
 
-        final HorizontalScrollView historyScroll = (HorizontalScrollView) findViewById(R.id.historyScroll);
+        final HorizontalScrollView historyScroll = findViewById(R.id.historyScroll);
         historyScroll.post(new Runnable() {
             public void run() {
                 historyScroll.smoothScrollTo(previousActionsLayout.getRight(), 0);
             }
         });
+
+        history.add(add);
+
+        setUndoButtonEnabled(true);
+    }
+
+    private void removeLastScore() {
+        int indexOfItemToRemove = history.size() - 1;
+        if(indexOfItemToRemove < 0)
+            return;
+
+        int lastScore = history.get(indexOfItemToRemove);
+        history.remove(indexOfItemToRemove);
+        currentScore -= lastScore;
+        updateScoreDisplay();
+
+        final LinearLayout previousActionsLayout = findViewById(R.id.previousActions);
+        previousActionsLayout.removeViewAt(indexOfItemToRemove);
+
+        setUndoButtonEnabled(history.size() > 0);
+    }
+
+    private void setUndoButtonEnabled(boolean enabled) {
+        ImageButton undoButton = findViewById(R.id.undoButton);
+
+        undoButton.setEnabled(enabled);
+        undoButton.setImageResource(enabled ? R.drawable.ic_backspace_black_36dp : R.drawable.ic_backspace_grey600_36dp);
     }
 
     private void setIsDoubled(boolean isDoubled) {
         this.isDoubled = isDoubled;
 
-        ImageButton button = (ImageButton)findViewById(R.id.doublerToggleButton);
+        ImageButton button = findViewById(R.id.doublerToggleButton);
         Drawable doublerImage = getResources().getDrawable(isDoubled? R.drawable.doubler_on : R.drawable.doubler_off);
         button.setImageDrawable(doublerImage);
 
@@ -82,7 +114,7 @@ public class CalculatorActivity extends Activity {
     private void setIsUsingRevaluationMarker(boolean isUsingRevaluationMarker) {
         this.isUsingRevaluationMarker = isUsingRevaluationMarker;
 
-        ImageButton button = (ImageButton)findViewById(R.id.revaluationToggleButton);
+        ImageButton button = findViewById(R.id.revaluationToggleButton);
         Drawable doublerImage = getResources().getDrawable(isUsingRevaluationMarker? R.drawable.revaluation_on : R.drawable.revaluation_off);
         button.setImageDrawable(doublerImage);
 
@@ -92,7 +124,7 @@ public class CalculatorActivity extends Activity {
     private void setIsUsingGermanRevaluationMarker(boolean isUsingGermanRevaluationMarker) {
         this.isUsingGermanRevaluationMarker = isUsingGermanRevaluationMarker;
 
-        ImageButton button = (ImageButton)findViewById(R.id.germanRevaluationToggleButton);
+        ImageButton button = findViewById(R.id.germanRevaluationToggleButton);
         Drawable doublerImage = getResources().getDrawable(isUsingGermanRevaluationMarker? R.drawable.german_revaluation_on : R.drawable.german_revaluation_off);
         button.setImageDrawable(doublerImage);
 
@@ -157,16 +189,20 @@ public class CalculatorActivity extends Activity {
 
     public void clearButton_Click(View view) {
         currentScore = 0;
+        history.clear();
         setIsDoubled(false);
         setIsUsingRevaluationMarker(false);
         setIsUsingGermanRevaluationMarker(false);
+        setUndoButtonEnabled(false);
         updateScoreDisplay();
 
-        LinearLayout previousActionsLayout = (LinearLayout) findViewById(R.id.previousActions);
+        LinearLayout previousActionsLayout = findViewById(R.id.previousActions);
         previousActionsLayout.removeAllViews();
     }
 
     private void showNumberPickerDialog(final boolean addToDisplayedScore, final int titleId) {
+        // https://possiblemobile.com/2013/05/layout-inflation-as-intended/
+        @SuppressLint("InflateParams")
         final NumberPicker numberPicker = (NumberPicker) getLayoutInflater().inflate(R.layout.number_picker_dialog_layout, null);
         numberPicker.setMaxValue(100);
         numberPicker.setMinValue(1);
@@ -221,6 +257,8 @@ public class CalculatorActivity extends Activity {
 
     private void showStartBonusCardsDialog(DrawnCards drawnCards) {
         LayoutInflater factory = LayoutInflater.from(CalculatorActivity.this);
+        // https://possiblemobile.com/2013/05/layout-inflation-as-intended/
+        @SuppressLint("InflateParams")
         final View dialogContent = factory.inflate(R.layout.random_start_bonus_cards_dialog, null);
         ((ImageView) dialogContent.findViewById(R.id.firstCard)).setImageResource(getDrawableIdForStartCard(drawnCards.firstCard()));
         ((ImageView) dialogContent.findViewById(R.id.secondCard)).setImageResource(getDrawableIdForStartCard(drawnCards.secondCard()));
@@ -255,13 +293,20 @@ public class CalculatorActivity extends Activity {
             notificationView.setViewVisibility(R.id.ifNotCoal, View.GONE);
         }
 
-        NotificationCompat.Builder notification = new NotificationCompat.Builder(this);
-        notification.setVisibility(NotificationCompat.VISIBILITY_PUBLIC);
-        notification.setSmallIcon(R.drawable.white);
-        notification.setContentIntent(openPendingIntent);
-        notification.setContent(notificationView);
-
+        String channelId = "notificationChannel";
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            CharSequence channelName = getString(R.string.notification_channel_name);
+            NotificationChannel mChannel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_LOW);
+            notificationManager.createNotificationChannel(mChannel);
+        }
+
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(this, channelId)
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+            .setSmallIcon(R.drawable.white)
+            .setContentIntent(openPendingIntent)
+            .setContent(notificationView);
+
         int notificationId = 1;
         notificationManager.notify(notificationId, notification.build());
     }
@@ -274,5 +319,9 @@ public class CalculatorActivity extends Activity {
 
     public void randomizeStartBonusCards_Click(View view) {
         showRandomStartBonusCards();
+    }
+
+    public void undoButton_Click(View view) {
+        removeLastScore();
     }
 }
